5b662330cf4e33600921d88ffa7ced2966e9b27c
rasadulalam <rashed464@outlook.com>
2022-02-07 14:48:11 +0600

    footer layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent} from './layout/header/header.component'
+import {FooterComponent} from './layout/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
