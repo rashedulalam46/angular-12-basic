34c083c47a9ba1ad93063f9eb8526e49281d1063
rasadulalam <rashed464@outlook.com>
2022-02-10 08:58:23 +0600

    footer layout changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent} from './layout/header/header.component'
-import {FooterComponent} from './layout/footer/footer.component'
+import { HeaderComponent } from './layout/header/header.component'
+import { FooterComponent } from './layout/footer/footer.component'
 
 @NgModule({
   declarations: [
