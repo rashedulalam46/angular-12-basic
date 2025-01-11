0b060a818e3e124c513ba1eee573a6b950afddc6
Rashedul Alam <rashed464@outlook.com>
2025-01-11 21:11:00 +0600

    Code updated

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
-
-
 @NgModule({
   declarations: [
     HeaderComponent,
